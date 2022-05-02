2e9317a834fc8bc287ae822c8a4d8cf99e71ad7e
LOGESH <95044430+LOGI-LAB@users.noreply.github.com>
2022-05-03 00:16:29 +0530

    LG

--- LG/modules/credits.py
+++ LG/modules/credits.py
@@ -4,9 +4,9 @@ from LG import app
 from LG.core.decorators.errors import capture_err
 from LG.utils.http import get
 
-__MODULE__ = "🦋 ᴄʀᴇᴅɪᴛꜱ ᴛᴏ 🦋"
+__MODULE__ = "🦋 ᴄʀᴇᴅɪᴛꜱ "
 __HELP__ = """""    ʜɪ ᴍʏ ɴᴀᴍᴇ ɪꜱ ʟᴏɢᴇꜱʜ . ᴛʜɪꜱ ʙᴏᴛ ʀᴇᴘᴏ ᴄʀᴇᴀᴛᴏʀ .
-                    ꜰᴏʀ ᴍᴏʀᴇ ʙᴏᴛ ʀᴇᴘᴏꜱ ꜱᴜʙꜱᴄʀɪʙᴇ     - @logi_channel
-                    ꜰᴏʀ ᴍᴏʀᴇ ᴜꜱᴇꜰᴜʟ ʙᴏᴛ ꜱᴜʙꜱᴄʀɪʙᴇ    - @lgbots
-                    ꜰᴏʀ ʟᴇᴀʀɴ ʜᴀᴄᴋɪɴɢ ᴊᴏɪɴ ꜰᴀꜱᴛ      - @hacker_x_x
-                    ᴀʙᴏᴜᴛ ᴍᴇ                         - @aboutlogesh """
+                    ꜰᴏʀ ᴍᴏʀᴇ ʙᴏᴛ ʀᴇᴘᴏꜱ ꜱᴜʙꜱᴄʀɪʙᴇ                @logi_channel
+                    ꜰᴏʀ ᴍᴏʀᴇ ᴜꜱᴇꜰᴜʟ ʙᴏᴛ ꜱᴜʙꜱᴄʀɪʙᴇ               @lgbots
+                    ꜰᴏʀ ʟᴇᴀʀɴ ʜᴀᴄᴋɪɴɢ ᴊᴏɪɴ ꜰᴀꜱᴛ                @hacker_x_x
+                    ᴀʙᴏᴜᴛ ᴍᴇ                                  @aboutlogesh """
